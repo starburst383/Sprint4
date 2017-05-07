667a4f295465825e6798d31e2d083adc1d2a8797
dphan1994 <dphan7743@gmail.com>
2017-05-06 19:05:16 -0500

    Update ChronoTimerTests.java

--- ChronoTimerTests.java
+++ ChronoTimerTests.java
@@ -1,22 +1,22 @@
 package Sprint3;
 
 //import Sprint3.*;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+//import org.jboss.arquillian.container.test.api.Deployment;
+//import org.jboss.arquillian.junit.Arquillian;
+//import org.jboss.shrinkwrap.api.ShrinkWrap;
+//import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+//import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by Owner on 5/6/2017.
  */
-@RunWith(Arquillian.class)
+//@RunWith(Arquillian.class)
 public class ChronoTimerTests {
     @Before
     public void setUp() throws Exception {
@@ -50,7 +50,7 @@ public class ChronoTimerTests {
         assertFalse(a.getPower());
     }
 
-     @Test
+    @Test
     public void event() throws Exception {
         ChronoTimerFORTESTSONLY a = new ChronoTimerFORTESTSONLY();
         a.setPower();
@@ -103,10 +103,7 @@ public class ChronoTimerTests {
         ChronoTimerFORTESTSONLY a = new ChronoTimerFORTESTSONLY();
         a.setPower();
         //turn all channel on
-        for(int i = 0; i < 8; ++i) {
-            a.togChannel(i);
-            assertTrue(a.channels[i]);
-        }
+        loop(a);
         //turn all channels off
         for(int i = 0; i < 8; ++i) {
             a.togChannel(i);
@@ -119,10 +116,7 @@ public class ChronoTimerTests {
         ChronoTimerFORTESTSONLY a = new ChronoTimerFORTESTSONLY();
         a.setPower();
         //turn all channles on
-        for(int i = 0; i < 8; ++i) {
-            a.togChannel(i);
-            assertTrue(a.channels[i]);
-        }
+        loop(a);
         //reset all channels
         a.reset();
         //test their value
@@ -142,17 +136,14 @@ public class ChronoTimerTests {
 
     }
 
-    
+
     @Test
     public void num() throws Exception {
         ChronoTimerFORTESTSONLY a = new ChronoTimerFORTESTSONLY();
         a.setPower();
         a.event("IND");
         a.newRun();
-        for(int i = 0; i < 8; ++i) {
-            a.togChannel(i);
-            assertTrue(a.channels[i]);
-        }
+        loop(a);
 
         a.num(11);
         a.num(12);
@@ -166,16 +157,16 @@ public class ChronoTimerTests {
         }
     }
 
-     @Test
-    public void trigger() throws Exception {
+    @Test
+    public void triggerStartFinish() throws Exception {
         ChronoTimerFORTESTSONLY a = new ChronoTimerFORTESTSONLY();
         a.setPower();
+        // for individual event
         a.event("IND");
         a.newRun();
-        for(int i = 0; i < 8; ++i) {
-            a.togChannel(i);
-            assertTrue(a.channels[i]);
-        }
+
+        loop(a);
+
         a.num(11);
         a.num(12);
         a.num(13);
@@ -194,7 +185,16 @@ public class ChronoTimerTests {
         a.trigChannel(8);
 
         assertEquals(a.totRacers, a.completed.size());
-
+        a.reset();
+        // for parallel individual event
+        a.event("PARIND");
+        a.newRun();
     }
 
+    public void loop (ChronoTimerFORTESTSONLY a) {
+        for(int i = 0; i < 8; ++i) {
+            a.togChannel(i);
+            assertTrue(a.channels[i]);
+        }
+    }
 }
