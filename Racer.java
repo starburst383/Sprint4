58cce3a2852cbe5751c2e231ae87d0518c4a102f
ShawnStrong <shawnpstrong@gmail.com>
2017-05-07 15:34:16 -0500

    Update Racer.java

--- Racer.java
+++ Racer.java
@@ -3,15 +3,27 @@ public class Racer {
 	public long start;
 	public long fin;
 	public int index;
+	public long time;
+	public String name;
 
 	
 	public Racer (int rNum, int channel) {
 		
 		this.racerNum = rNum;
-
+		this.name = "";
 		this.start = 0;
 		this.fin = 0;
 		this.index = channel;
+		this.time = 0;
+	}
+	
+	public Racer (int rNum, String alias){
+		this.racerNum = rNum;
+		this.name = alias;
+	}
+	
+	public void printFileRacers(){
+		System.out.println(this.racerNum + " " + this.name);
 	}
 
 }
