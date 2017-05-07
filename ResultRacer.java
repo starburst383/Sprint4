4c360f4513ee36999626a4bfe897c03af2264bcc
ShawnStrong <shawnpstrong@gmail.com>
2017-05-07 15:46:51 -0500

    Create ResultRacer.java

---
+++ ResultRacer.java
@@ -0,0 +1,17 @@
+public class ResultRacer {
+	public int Place;
+	public int bibNumber;
+	public String Name;
+	public long Time;
+
+	
+	public ResultRacer (int i, int num, String alias, long tim) {
+		
+		this.Place = i;
+		this.bibNumber = num;
+		this.Name = alias;
+		this.Time = tim;
+	}
+
+
+}
